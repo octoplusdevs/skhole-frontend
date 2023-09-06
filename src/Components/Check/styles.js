485d4d0ea3f0bea0a0c8bf650fe3944112724970
mraguinaldo <cesaraguinaldo23@gmail.com>
2023-09-06 15:05:19 +0100

    Changes in the checkbox

--- src/Components/Check/styles.js
+++ src/Components/Check/styles.js
@@ -4,7 +4,8 @@ export const Wrapper = styled.label`
   display: block;
   position: relative;
   padding-left: 35px;
-  margin-bottom: 12px;
+  height: 24px;
+  width: 24px;
   cursor: pointer;
   font-size: 22px;
   -webkit-user-select: none;
@@ -18,16 +19,25 @@ export const Wrapper = styled.label`
     height: 0;
     width: 0;
   }
+  .checkbox{
+    display: flex;
+    position: relative;
+    svg{
+      position: absolute;
+      left: -32px;
+      top: 2px;
+      z-index: 2;
+    }
+  }
   .checkmark {
     position: absolute;
     transition: all 0.5s ease;
 
     top: 0;
     left: 0;
-    height: 20px;
-    width: 20px;
+    height: 24px;
+    width: 24px;
     background-color: #222f35;
-    border-radius: 50%;
   }
   &:hover input ~ .checkmark {
     background-color: #ccc;
