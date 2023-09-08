be5eebd600d90bec688f3da11e69a82c7b6a43c1
danguya <daniel.yava16@gmail.com>
2023-09-08 22:17:59 +0100

    fix bug refresh token

--- src/Routes/private.jsx
+++ src/Routes/private.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {getAuthToken} from '../utils/auth'
+import { getAuthToken } from "../utils/auth";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function PrivateRoute() {
-  const isAuthenticated = getAuthToken().accessToken != null;
+  const isAuthenticated = getAuthToken().accessToken || getAuthToken().refreshToken != null;
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 }
