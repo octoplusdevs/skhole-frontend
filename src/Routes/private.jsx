d8ed530a8c20ee20576ad6e13c7b7bf2f13b2d68
danguya <daniel.yava16@gmail.com>
2023-09-08 22:53:18 +0100

    refactoring private routes

--- src/Routes/private.jsx
+++ src/Routes/private.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { getAuthToken } from "../utils/auth";
 import { Navigate, Outlet } from "react-router-dom";
+import { getAuthToken } from "../utils/auth";
 
-export function PrivateRoute() {
-  const isAuthenticated = getAuthToken().accessToken || getAuthToken().refreshToken != null;
+const PrivateRoute = () => {
+  const { accessToken, refreshToken } = getAuthToken();
+
+  const isAuthenticated = accessToken != null || refreshToken != null;
+
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-}
+};
+
+export default PrivateRoute;
