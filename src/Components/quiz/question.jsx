b9d1549db414508b9f43e45817108c4a8431c25e
danguya <daniel.yava16@gmail.com>
2024-01-27 08:34:47 -0500

    style: ajustando o tamanho da fonte

--- src/Components/quiz/question.jsx
+++ src/Components/quiz/question.jsx
@@ -1,6 +1,6 @@
 function Question({ question_text }){
   return(
-    <p className="text-white text-[18px] font-medium">
+    <p className="text-white text-[14px] sm:text-[16px] font-medium">
       { question_text }
     </p>
   )
