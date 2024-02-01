504a9ef51b26909b6de5efa4bc95f02670c1a787
danguya <daniel.yava16@gmail.com>
2024-02-01 17:03:37 -0500

    feat: bloqueando a selecao de texto em pontos

--- src/Components/Header/points.jsx
+++ src/Components/Header/points.jsx
@@ -8,7 +8,7 @@ function UserPoints() {
 
   return (
     <div
-      className="bg-[#333A3D] flex gap-2 items-center justify-center px-4 py-3 h-max rounded-[6px]"
+      className="bg-[#333A3D] select-none pointer-events-none flex gap-2 items-center justify-center px-4 py-3 h-max rounded-[6px]"
     >
       <span className="text-white text-[18px] font-medium sm:text-[20px]">
         {userInfo?.points}
