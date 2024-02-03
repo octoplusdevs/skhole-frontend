3a0a8c7542194e6eeb3bd69caf9c29f5249fd03f
danguya <daniel.yava16@gmail.com>
2024-02-03 09:27:23 -0500

    monstrando wrapper

--- src/pages/Watch/index.jsx
+++ src/pages/Watch/index.jsx
@@ -1,6 +1,6 @@
 // import { Suspense, lazy } from "react";
 import { useParams } from "react-router-dom";
-// import { Wrapper } from "./style";
+import { Wrapper } from "./style";
 // import Player from "../../Components/Player";
 import { useVideo } from "../../hooks/useVideo";
 // import { Link, Student } from "@phosphor-icons/react";
@@ -16,65 +16,60 @@ export default function Watch() {
   //  const allVideos = modules.reduce((acc, module) => [...acc, ...module.videos], []);
   // console.log(video);
   return (
-    <h1>{video?.title}</h1>
-    // <Wrapper className="flex flex-col gap-14">
-    //   <div className="grid">
-    //     <div className="main">
-    //       <Player
-    //         title={video?.title}
-    //         description={video?.description}
-    //         duration={video?.duration}
-    //         url={video?.url}
-    //         videoIdCDN={video?.videoIdCDN}
-    //         video_id={video?.id}
-    //         isLoading={isLoadingVideo}
-    //         initialLastPosition={video?.progress?.lastPosition}
-    //       />
-    //       <div className={`${isLoadingVideo || video?.length <= 0 ? "skeleton" : "video__info"}`}>
-    //         <h1>{video?.title}</h1>
-    //         <p>{video?.description}</p>
-    //       </div>
-    //     </div>
-    //     {/* <div className="playlist"> */}
-    //     <aside className="aside">
-    //       <Suspense fallback={<h1>Carregando a playlist</h1>}>
-    //         <Playlist
-    //           // modules={modules}
-    //           // slug_course={slug_course}
-    //           activeVideo={slug_video || video?.slug}
-    //         // status={status}
-    //         />
-    //       </Suspense>
-    //       {video?.assetLink ?
-    //         <a href={video.assetLink} target="_blank" rel="noopener noreferrer" className="button_assets">
-    //           <Link size={24} weight="bold" />
-    //           <span>
-    //             Recursos da Aula
-    //           </span>
-    //         </a>
-    //         :
-    //         <span title="Ainda não disponível." className="button_assets disabled">
-    //           <Link size={24} weight="regular" />
-    //           <span>
-    //             Recursos da Aula
-    //           </span>
-    //         </span>
-    //       }
-    //       <span title="Ainda não disponível." className="button_assets disabled">
-    //         <Student size={24} weight="regular" />
-    //         <span>
-    //           Emitir Certificado
-    //         </span>
-    //       </span>
+    <Wrapper className="flex flex-col gap-14">
+      <h1>{video?.title}</h1>
+      {/* <div className="grid">
+        <div className="main">
+          <Player
+            title={video?.title}
+            description={video?.description}
+            duration={video?.duration}
+            url={video?.url}
+            videoIdCDN={video?.videoIdCDN}
+            video_id={video?.id}
+            isLoading={isLoadingVideo}
+            initialLastPosition={video?.progress?.lastPosition}
+          />
+          <div className={`${isLoadingVideo || video?.length <= 0 ? "skeleton" : "video__info"}`}>
+            <h1>{video?.title}</h1>
+            <p>{video?.description}</p>
+          </div>
+        </div>
+        <aside className="aside">
+          <Suspense fallback={<h1>Carregando a playlist</h1>}>
+            <Playlist
+              activeVideo={slug_video || video?.slug}
+            />
+          </Suspense>
+          {video?.assetLink ?
+            <a href={video.assetLink} target="_blank" rel="noopener noreferrer" className="button_assets">
+              <Link size={24} weight="bold" />
+              <span>
+                Recursos da Aula
+              </span>
+            </a>
+            :
+            <span title="Ainda não disponível." className="button_assets disabled">
+              <Link size={24} weight="regular" />
+              <span>
+                Recursos da Aula
+              </span>
+            </span>
+          }
+          <span title="Ainda não disponível." className="button_assets disabled">
+            <Student size={24} weight="regular" />
+            <span>
+              Emitir Certificado
+            </span>
+          </span>
 
 
-    //     </aside>
-    //     {/* </div> */}
-    //   </div>
+        </aside>
+      </div>
 
-    //   <Suspense fallback={<h1>Carregando</h1>}>
-    //     <Quiz QUESTIONS={video?.questions}/>
-    //   </Suspense>
-    // </Wrapper>
+      <Suspense fallback={<h1>Carregando</h1>}>
+        <Quiz QUESTIONS={video?.questions}/>
+      </Suspense> */}
+    </Wrapper>
   );
 }
