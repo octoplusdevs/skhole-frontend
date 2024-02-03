f054fd7b66823054b388c9bb0b4345cab096aba3
danguya <daniel.yava16@gmail.com>
2024-02-03 09:00:33 -0500

    ocultando todos os components

--- src/pages/Watch/index.jsx
+++ src/pages/Watch/index.jsx
@@ -1,79 +1,80 @@
-import { useParams } from "react-router-dom";
-import { Wrapper } from "./style";
-import Player from "../../Components/Player";
-import { useVideo } from "../../hooks/useVideo";
-// import { Link, Student } from "@phosphor-icons/react";
 // import { Suspense, lazy } from "react";
+// import { useParams } from "react-router-dom";
+// import { Wrapper } from "./style";
+// import Player from "../../Components/Player";
+// import { useVideo } from "../../hooks/useVideo";
+// import { Link, Student } from "@phosphor-icons/react";
 
 // const Playlist = lazy(() => import("../../Components/Playlist"))
 // const Quiz = lazy(() => import("../../Components/quiz"))
 
 export default function Watch() {
-  const { slug_course, slug_video, slug_module } = useParams();
-  const { data: video, isLoading: isLoadingVideo } = useVideo("pixel-perfect-basico", slug_module, slug_video);
+  // const { slug_course, slug_video, slug_module } = useParams();
+  // const { data: video, isLoading: isLoadingVideo } = useVideo("pixel-perfect-basico", slug_module, slug_video);
 
   // Obtenha todos os vídeos dos módulos
   //  const allVideos = modules.reduce((acc, module) => [...acc, ...module.videos], []);
   // console.log(video);
   return (
-    <Wrapper className="flex flex-col gap-14">
-      <div className="grid">
-        <div className="main">
-          <Player
-            title={video?.title}
-            description={video?.description}
-            duration={video?.duration}
-            url={video?.url}
-            videoIdCDN={video?.videoIdCDN}
-            video_id={video?.id}
-            isLoading={isLoadingVideo}
-            initialLastPosition={video?.progress?.lastPosition}
-          />
-          <div className={`${isLoadingVideo || video?.length <= 0 ? "skeleton" : "video__info"}`}>
-            <h1>{video?.title}</h1>
-            <p>{video?.description}</p>
-          </div>
-        </div>
-        {/* <div className="playlist"> */}
-        <aside className="aside">
-          {/* <Suspense fallback={<h1>Carregando a playlist</h1>}>
-            <Playlist
-              // modules={modules}
-              // slug_course={slug_course}
-              activeVideo={slug_video || video?.slug}
-            // status={status}
-            />
-          </Suspense> */}
-          {video?.assetLink ?
-            <a href={video.assetLink} target="_blank" rel="noopener noreferrer" className="button_assets">
-              <Link size={24} weight="bold" />
-              <span>
-                Recursos da Aula
-              </span>
-            </a>
-            :
-            <span title="Ainda não disponível." className="button_assets disabled">
-              {/* <Link size={24} weight="regular" /> */}
-              <span>
-                Recursos da Aula
-              </span>
-            </span>
-          }
-          <span title="Ainda não disponível." className="button_assets disabled">
-            {/* <Student size={24} weight="regular" /> */}
-            <span>
-              Emitir Certificado
-            </span>
-          </span>
+    <h1>Watching...</h1>
+    // <Wrapper className="flex flex-col gap-14">
+    //   <div className="grid">
+    //     <div className="main">
+    //       <Player
+    //         title={video?.title}
+    //         description={video?.description}
+    //         duration={video?.duration}
+    //         url={video?.url}
+    //         videoIdCDN={video?.videoIdCDN}
+    //         video_id={video?.id}
+    //         isLoading={isLoadingVideo}
+    //         initialLastPosition={video?.progress?.lastPosition}
+    //       />
+    //       <div className={`${isLoadingVideo || video?.length <= 0 ? "skeleton" : "video__info"}`}>
+    //         <h1>{video?.title}</h1>
+    //         <p>{video?.description}</p>
+    //       </div>
+    //     </div>
+    //     {/* <div className="playlist"> */}
+    //     <aside className="aside">
+    //       <Suspense fallback={<h1>Carregando a playlist</h1>}>
+    //         <Playlist
+    //           // modules={modules}
+    //           // slug_course={slug_course}
+    //           activeVideo={slug_video || video?.slug}
+    //         // status={status}
+    //         />
+    //       </Suspense>
+    //       {video?.assetLink ?
+    //         <a href={video.assetLink} target="_blank" rel="noopener noreferrer" className="button_assets">
+    //           <Link size={24} weight="bold" />
+    //           <span>
+    //             Recursos da Aula
+    //           </span>
+    //         </a>
+    //         :
+    //         <span title="Ainda não disponível." className="button_assets disabled">
+    //           <Link size={24} weight="regular" />
+    //           <span>
+    //             Recursos da Aula
+    //           </span>
+    //         </span>
+    //       }
+    //       <span title="Ainda não disponível." className="button_assets disabled">
+    //         <Student size={24} weight="regular" />
+    //         <span>
+    //           Emitir Certificado
+    //         </span>
+    //       </span>
 
 
-        </aside>
-        {/* </div> */}
-      </div>
+    //     </aside>
+    //     {/* </div> */}
+    //   </div>
 
-      {/* <Suspense fallback={<h1>Carregando</h1>}>
-        <Quiz QUESTIONS={video?.questions}/>
-      </Suspense> */}
-    </Wrapper>
+    //   <Suspense fallback={<h1>Carregando</h1>}>
+    //     <Quiz QUESTIONS={video?.questions}/>
+    //   </Suspense>
+    // </Wrapper>
   );
 }
